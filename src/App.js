ce38daff60d8675a7e70594d7836939e4d7dfe8d
Abhishek Yadav <yadavabhishek60477@gmail.com>
2025-06-15 19:04:33 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isCompleteScreen,setIsCompletedScreen]=useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App'>
+      <h1>My Todo</h1> 
+      <div className='todo-wrapper'>
+        <div className='todo-input'> 
+          <div className='todo-input-item'>
+            <label>Title</label>
+            <input type='text' placeholder='what is task title?'></input>
+          </div>
+          <div className='todo-input-item'>
+            <label>Description</label>
+            <input type='text' placeholder='what is task title?'></input>
+          </div>
+          <div className='todo-input-item'>
+            <button type='button' className='primaryBtn'>Add</button>
+          </div>
+              </div>
+              <div className='btn-area'>
+            <button className = {`secondaryBtn ${isCompleteScreen ===false && 'active'}`} onClick={()=>setIsCompletedScreen(false)}>Todo</button>
+            <button className={`secondaryBtn ${isCompleteScreen ===false && 'active'}`}onClick={()=>setIsCompletedScreen(true)}>Completed</button>
+            
+          </div>
+           <div className='todo-list'>
+             <div className='todo-list-item'>
+                <h3>Task1</h3>
+                <p>Description</p> 
+            </div>
+            
+        </div>
+      </div>
+    </div>  
   );
 }
 
